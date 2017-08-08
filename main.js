679427d619d9c4e459d5b256315ea18e0938a102
Shannon <reidy.pf@gmail.com>
2017-08-08 15:30:31 -0500

    still some styling to be done

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ submit.addEventListener('click', function(){
 
 });
 function searching(ourInput){
-let searchURL = "http://recipepuppyproxy.herokuapp.com/api/?q=" + ourInput;
+fixedInput = ourInput.replace(/ /i, '&');
+let searchURL = "http://recipepuppyproxy.herokuapp.com/api/?q=" + fixedInput;
+console.log(searchURL);
 fetch(searchURL)
   .then(function(response){
     if(response.status !== 200){
@@ -28,8 +30,30 @@ fetch(searchURL)
 
 }).then(function(data){
   console.log(data);
-  data.results.forEach(function(recipecard){
-    console.log(recipecard.title);
+  data.results.forEach(function(recipeInfo){
+    console.log(recipeInfo.title);
+    if (recipeInfo.thumbnail !== '') {
+      let recipecard = `
+      <img src=${recipeInfo.thumbnail}>
+      <p>${recipeInfo.title}</p>
+      <a href = ${recipeInfo.href}>See the recipe</a>`
+
+    let newrecipecard = document.createElement('div');
+    newrecipecard.className = 'recipe';
+    newrecipecard.innerHTML = recipecard;
+    document.querySelector('.container').appendChild(newrecipecard);
+    }
+    else{
+      let recipecard = `
+      <img src="http://via.placeholder.com/100x100">
+      <p>${recipeInfo.title}</p>
+      <a href = ${recipeInfo.href}>See the recipe</a>`
+
+    let newrecipecard = document.createElement('div');
+    newrecipecard.className = 'recipe';
+    newrecipecard.innerHTML = recipecard;
+    document.querySelector('.container').appendChild(newrecipecard);
+    }
   })
 })
 }
